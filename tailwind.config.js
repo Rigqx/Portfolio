ec5010a57281648e0a8cffa488a744d67346881b
Rigqx <rigqx1337@protonmail.com>
2025-05-31 04:01:05 +0200

    shit doesnt work

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+// tailwind.config.cjs
+module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
